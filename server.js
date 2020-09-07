c3bb119575d8338aff4c19d022d9cf8738d363c8
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-07 10:11:54 -0400

    imported modules from users.js to server

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
