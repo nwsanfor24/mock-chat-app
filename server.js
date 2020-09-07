3ed6449ddc468259a46d572b0d26f38dd003f21b
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-07 09:39:29 -0400

    created a util folder that has a function to create msg as an user with a date and time then connected it to the server

--- server.js
+++ server.js
@@ -2,31 +2,35 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatCord Bot';
 
 //Run when client connects
 io.on('connection', socket => {
 
     //Welcome current user
-    socket.emit('message', 'Welcome to the Chat App!');
+    socket.emit('message', formatMessage(botName, 'Welcome to the Chat App!'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName,'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 
