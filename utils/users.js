f8137974795d03a24924fa66fa44f99ef176d62c
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-07 10:06:49 -0400

    made function to join user to chat

---
+++ utils/users.js
@@ -0,0 +1,12 @@
+const users = [];
+
+//Join user to chat
+function userJoin(id, username, room) {
+    const user = { id, username, room };
+
+    users.push(user);
+
+    return(user);
+}
+
+//Get current user
