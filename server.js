146833d4dab65c95e34a14f7da6445442d4344dc
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-07 20:19:47 -0400

    server works now

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const {
     userLeave,
     getRoomUsers
 } = require('./utils/users');
+const { isPrimitive } = require('util');
 
 const app = express();
 const server = http.createServer(app);
@@ -25,7 +26,49 @@ io.on('connection', socket =>{
     socket.on('joinRoom', ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
 
-        socket.join(user.room)
+        socket.join(user.room);
+
+        //Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to the Chat!'));
+
+        //Broadcast when a user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined the chat`)
+            );
+
+        //Send user and room infor
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+        //Listen for chatMessage
+        socket.on('chatMessage', msg => {
+            const user = getCurrentUser(socket.id);
+
+            io.to(user.room).emit('message', formatMessage(user.username, msg));
+        });
+
+        //Runs when client disconnects 
+        socket.on('disconnect', () => {
+            const user = userLeave(socket.id);
+
+            if (user) {
+                io.to(user.room).emit(
+                    'message',
+                    formatMessage(botName, `${user.username} has left the chat`)
+                );
+
+                //Send users and room info
+                io.to(user.room).emit('roomUsers', {
+                    room: user.room,
+                    users: getRoomUsers(user.room)
+                });
+            }            
+        });
     });
 });
 
